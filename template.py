cdc36e5bb2989fd15ce7ab21f1a2f672e16221a2
Roffin7 <roffinjason@gmail.com>
2024-01-10 00:02:11 +0530

    folder_structure

--- template.py
+++ template.py
@@ -15,14 +15,14 @@ list_of_files=[
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception/exception"
+    "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
     "setup.py",
-    "setup.config",
+    "setup.cfg",
     "pyproject.toml",
     "tox.ini",
     "experiment/experiments.ipynb"  
